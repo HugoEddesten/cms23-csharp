b8b0e68b9ae2c8bb1898ce4dc688216a0d43d7ed
HugoEddesten <hugo.eddesten@yh.nakademin.se>
2023-09-10 18:05:12 +0200

    Cleaning upp and adding things

--- Exercises/ClassesExercise/SemiGraphicUI/Program.cs
+++ Exercises/ClassesExercise/SemiGraphicUI/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace SemiGraphicUI
+﻿using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace SemiGraphicUI
 {
     internal class Program
     {
@@ -21,6 +24,7 @@
             testObject2.Email = "bengt.lundgren1@gmail.com";
             testObject2.PhoneNumber = "0737516336";
             contacts.Add(testObject2);
+            
 
 
 
@@ -28,66 +32,131 @@
 
 
 
-            int currentChoice = 1;
+            
             ConsoleKeyInfo keyPress;
             bool programIsOn = true;
 
 
             while (programIsOn == true)
             {
-                currentChoice = 1;
+                int mainMenuChoice = 0;
 
 
 
                 do
                 {
-                    Console.Clear();
-                    if (currentChoice == 1)
-                    {
-                        Console.WriteLine("Your contact list:");
+                    // Displays the main menu 
+                    MainMenu(mainMenuChoice);
 
-                        Console.WriteLine("=> {Display contacts}");
-                        Console.WriteLine("2: Add contact");
-                        Console.WriteLine("0: Exit");
-                    }
-                    else if (currentChoice == 2)
-                    {
-                        Console.WriteLine("Your contact list:");
-
-                        Console.WriteLine("1: Display contacts");
-                        Console.WriteLine("=> {Add contact}");
-                        Console.WriteLine("0: Exit");
-                    }
-                    else if (currentChoice == 3)
-                    {
-                        Console.WriteLine("Your contact list:");
-
-                        Console.WriteLine("1: Display contacts");
-                        Console.WriteLine("2: Add contact");
-                        Console.WriteLine("=> {Exit}");
-                    }
-
-
-
+                    // Reads input and keeps count of which option is selected
                     keyPress = Console.ReadKey();
-                    if (keyPress.Key == ConsoleKey.DownArrow && currentChoice < 3)
-                    {
-                        currentChoice++;
-                    }
-                    else if (keyPress.Key == ConsoleKey.UpArrow && currentChoice > 1)
-                    {
-                        currentChoice--;
-                    }
-                    //Console.WriteLine(currentChoice);
+                    mainMenuChoice = CheckKeyInput(keyPress, mainMenuChoice, 3);
+
                 } while (keyPress.Key != ConsoleKey.Enter);
-               
-                switch (currentChoice)
+
+                switch (mainMenuChoice)
                 {
-                    case 2:
+
+                    case 0:
+
+
+                        int contactChoice = 0;
+                        do
+                        {
+                            Console.Clear();
+                            
+                            Console.WriteLine("Your contacts:|");
+                            Console.WriteLine("--------------+\n");
+
+
+                            if (contacts.Count > 0)
+                            {
+                                // Loops through the list contacts and displays each contact by name
+                                ContactMenu(contacts, contactChoice);
+
+                                // Reads input and keeps count of which option is selected
+                                keyPress = Console.ReadKey();
+                                contactChoice = CheckKeyInput(keyPress, contactChoice, contacts.Count);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Well... this place is empty! Add a contact to see them here.");
+                                Console.WriteLine("Create a Contact?");
+
+                                keyPress = Console.ReadKey();
+                                if (keyPress.Key == ConsoleKey.Backspace)
+                                    break;
+                                else if (keyPress.Key == ConsoleKey.Enter)
+                                {
+                                    goto case 1;
+                                }
+                            }
+
+                        } while (keyPress.Key != ConsoleKey.Enter && keyPress.Key != ConsoleKey.Backspace);
+                        if (keyPress.Key == ConsoleKey.Backspace)
+                            break;
+                        else
+                        {
+
+                            int propertyChoice = 0;
+                            do
+                            {
+                                // Displays the menu where the user chooses between contacts
+                                PropertyMenu(contacts[contactChoice], propertyChoice);
+
+
+                                // Reads input and keeps count of which option is selected
+                                keyPress = Console.ReadKey();
+                                propertyChoice = CheckKeyInput(keyPress, propertyChoice, 5);
+
+                            } while (keyPress.Key != ConsoleKey.Enter && keyPress.Key != ConsoleKey.Backspace);
+
+                            if (keyPress.Key == ConsoleKey.Backspace)
+                            {
+                                goto case 0;
+                            } 
+                            else
+                            {
+                                Contact contact = contacts[contactChoice];
+                                switch (propertyChoice)
+                                {
+                                    case 0:
+                                        Console.Clear();
+                                        Console.Write("First Name: ");
+                                        contact.FirstName = Console.ReadLine() ?? "";
+                                        break;
+                                    case 1:
+                                        Console.Clear();
+                                        Console.Write("Last Name: ");
+                                        contact.LastName = Console.ReadLine() ?? "";
+                                        break;
+                                    case 2:
+                                        Console.Clear();
+                                        Console.Write("Address: ");
+                                        contact.Address = Console.ReadLine() ?? "";
+                                        break;
+                                    case 3:
+                                        Console.Clear();
+                                        Console.Write("Email: ");
+                                        contact.Email = Console.ReadLine() ?? "";
+                                        break;
+                                    case 4:
+                                        Console.Clear();
+                                        Console.Write("Phone number: ");
+                                        contact.PhoneNumber = Console.ReadLine() ?? "";
+                                        break;
+                                }
+                            }
+                            
+                        }   
+                        break;
+                    case 1:
                         Console.Clear();
+                        Console.WriteLine("Create a new contact|");
+                        Console.WriteLine("--------------------+\n");
                         AddContact(contacts);
                         break;
-                    case 3:
+                    case 2:
                         programIsOn = false;
                         break;
 
@@ -122,6 +191,148 @@
 
 
         }
+
+
+        public static void DisplayContact(Contact contact, bool isEnumerated = false)
+        {
+            if (isEnumerated == true)
+            {
+                
+                Console.WriteLine($"1: {contact.FirstName}");
+                Console.WriteLine($"2: {contact.LastName}");
+                Console.WriteLine($"3: {contact.Address}");
+                Console.WriteLine($"4: {contact.Email}");
+                Console.WriteLine($"5: {contact.PhoneNumber}");
+
+            }
+            else
+            {
+                
+                Console.WriteLine($"First Name:     {contact.FirstName}");
+                Console.WriteLine($"Last Name:      {contact.LastName}");
+                Console.WriteLine($"Address:        {contact.Address}");
+                Console.WriteLine($"Email:          {contact.Email}");
+                Console.WriteLine($"Phone Number:   {contact.PhoneNumber}");
+
+            }
+
+        }
+
+        public static int CheckKeyInput(ConsoleKeyInfo keyPress, int currentChoice, int numberOfChoices)
+        {
+            if (keyPress.Key == ConsoleKey.DownArrow && currentChoice < numberOfChoices-1)
+            {
+                currentChoice++;
+            }
+            else if (keyPress.Key == ConsoleKey.UpArrow && currentChoice > 0)
+            {
+                currentChoice--;
+            }
+            return currentChoice;
+        }
+
+        public static void MainMenu(int mainMenuChoice)
+        {
+            Console.Clear();
+            if (mainMenuChoice == 0)
+            {
+                Console.WriteLine("Your contact list:|");
+                Console.WriteLine("------------------+");
+
+                Console.WriteLine("  +----------------+");
+                Console.WriteLine("  |Display contacts|");
+                Console.WriteLine("  +----------------+");
+
+                Console.WriteLine("   Add contact\n");
+
+                Console.WriteLine("   Exit");
+            }
+            else if (mainMenuChoice == 1)
+            {
+                Console.WriteLine("Your contact list:|");
+                Console.WriteLine("------------------+\n");
+
+                Console.WriteLine("   Display contacts");
+                Console.WriteLine("  +-----------+");
+                Console.WriteLine("  |Add contact|");
+                Console.WriteLine("  +-----------+");
+                Console.WriteLine("   Exit");
+            }
+            else if (mainMenuChoice == 2)
+            {
+                Console.WriteLine("Your contact list:|");
+                Console.WriteLine("------------------+\n");
+
+                Console.WriteLine("   Display contacts\n");
+                Console.WriteLine("   Add contact");
+                Console.WriteLine("  +------+");
+                Console.WriteLine("  |Exit  |");
+                Console.WriteLine("  +------+\n");
+            }
+        }
+
+        public static void ContactMenu(List<Contact> contacts, int contactChoice)
+        {
+            for (int i = 0; i < contacts.Count; i++)
+            {
+                if (contactChoice == i)
+                {
+
+                    Console.WriteLine($" |{contacts[i].FirstName} {contacts[i].LastName}|");
+
+                }
+                else
+                    Console.WriteLine($"  {contacts[i].FirstName} {contacts[i].LastName}");
+            }
+        }
+
+        public static void PropertyMenu(Contact contact, int currentChoice)
+        {
+            Console.Clear();
+            if (currentChoice == 0)
+            {
+                Console.WriteLine($"[Edit]    =>    {contact.FirstName}");
+                Console.WriteLine($"Last Name:      {contact.LastName}");
+                Console.WriteLine($"Address:        {contact.Address}");
+                Console.WriteLine($"Email:          {contact.Email}");
+                Console.WriteLine($"Phone Number:   {contact.PhoneNumber}");
+            }
+            else if (currentChoice == 1)
+            {
+                Console.WriteLine($"First Name:     {contact.FirstName}");
+                Console.WriteLine($"[Edit]    =>    {contact.LastName}");
+                Console.WriteLine($"Address:        {contact.Address}");
+                Console.WriteLine($"Email:          {contact.Email}");
+                Console.WriteLine($"Phone Number:   {contact.PhoneNumber}");
+            }
+            else if (currentChoice == 2)
+            {
+                Console.WriteLine($"First Name:     {contact.FirstName}");
+                Console.WriteLine($"Last Name:      {contact.LastName}");
+                Console.WriteLine($"[Edit]    =>    {contact.Address}");
+                Console.WriteLine($"Email:          {contact.Email}");
+                Console.WriteLine($"Phone Number:   {contact.PhoneNumber}");
+            }
+            else if (currentChoice == 3)
+            {
+                Console.WriteLine($"First Name:     {contact.FirstName}");
+                Console.WriteLine($"Last Name:      {contact.LastName}");
+                Console.WriteLine($"Address:        {contact.Address}");
+                Console.WriteLine($"[Edit]    =>    {contact.Email}");
+                Console.WriteLine($"Phone Number:   {contact.PhoneNumber}");
+            }
+            else if (currentChoice == 4)
+            {
+                Console.WriteLine($"First Name:     {contact.FirstName}");
+                Console.WriteLine($"Last Name:      {contact.LastName}");
+                Console.WriteLine($"Address:        {contact.Address}");
+                Console.WriteLine($"Email:          {contact.Email}");
+                Console.WriteLine($"[Edit]    =>    {contact.PhoneNumber}");
+            }
+        }
+            
+        
+    
 
 
     }
