37cd9fe00f354ff6730fcc37c48b0080f08deaf5
HugoEddesten <hugo.eddesten@yh.nackademin.se>
2023-10-04 00:21:12 +0200

    Nästan klar

--- Exercises/Assignment_ConsoleApp/Assignment_ConsoleApp/Models/Contact.cs
+++ Exercises/Assignment_ConsoleApp/Assignment_ConsoleApp/Models/Contact.cs
@@ -5,9 +5,10 @@ namespace Assignment_ConsoleApp.Models;
 public class Contact : IContact
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-    public IAddress Address { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    public string FirstName { get; set; } = "Empty";
+    public string LastName { get; set; } = "Empty";
+    public string Email { get; set; } = "Empty";
+    public string Phone { get; set; } = "Empty";
+    public IAddress Address { get; set; } = null!;
 }
